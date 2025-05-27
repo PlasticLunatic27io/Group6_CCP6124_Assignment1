00736b2e6da8d83d9088d072f7e894bf2591c2f8
Adam Harris Nyirop <adamharrisnyirop@gmail.com>
2025-05-28 00:20:53 +0800

    28/5/2025 - My battlefield

--- T10L_Assignment_Group6.cpp
+++ T10L_Assignment_Group6.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 using namespace std;
 
+class Battlefield
+{
+    private:
+        int x;
+        int y;
+    public:
+        void tiles(int x, int y)
+        {
+            for (int h = 0; h < x + 2; h++)
+            {
+                cout << "#";
+            }
+            cout << endl;
+            for (int i = 0; i < y; i++)
+            {
+                cout << "#";
+                for (int j = 0; j < x; j++)
+                {
+                    cout << "_";
+                }
+                cout << "#";
+                cout << endl;
+            }
+            for (int k = 0; k < x + 2; k++)
+            {
+                cout << "#";
+            }
+        };
+};
+
 class Robot
 {
     private:
@@ -75,5 +105,16 @@ class TrackBot
 
 main()
 {
+    Battlefield field1;
+    int x;
+    int y;
+
+    cout << "Insert how many columns (x) does a battlefield need: ";
+    cin >> x;
+    cout << "\nInsert how many rows (y) does a battlefield need: ";
+    cin >> y;
+
+    field1.tiles(x, y);
+    
     return 0;
 }
